59c870ec117e0af70d58e683fa6a324ba52823ce
yuda-lyu <firsemisphere@gmail.com>
2026-09-18 17:27:38 +0800

    modify: update pks

--- dist/into-stream.umd.js
+++ dist/into-stream.umd.js
@@ -1,5 +1,5 @@
 /*!
- * into-stream v1.0.38
+ * into-stream v1.0.39
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- dist/through.umd.js
+++ dist/through.umd.js
@@ -1,5 +1,5 @@
 /*!
- * through v1.0.38
+ * through v1.0.39
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
